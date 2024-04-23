0df15f574f4fc4a544619e8f85d43633b8b9f112
taishi <wilas.tk09@gmail.com>
2024-04-23 10:49:37 +0900

    本番環境でhttpsでの遷移をさせるためにwebサーバへのアクセスを許可

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies=[
+      '172.16.3.0/24'
+    ];
 
     /**
      * The headers that should be used to detect proxies.
